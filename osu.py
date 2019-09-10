303c9ad0c1b7d1627904c1700369b3e19d320de7
Andrew Mariano <corim.corim@gmail.com>
2019-09-09 20:33:41 -0700

    yikes

--- osu.py
+++ osu.py
@@ -1,7 +1,7 @@
 import requests
 import json
+from api_key import api_key
 
-api_key = "e4768ff59492b8e6008b1dc97db82f59dc63e48e"
 
 def get_user_info(user):
     payload = {'k': api_key, 'u': user}
