a51b80ec9760b4f23f7890732a00713bcac46045
Andrew Mariano <corim.corim@gmail.com>
2019-09-10 20:33:39 -0700

    Rename api_key var to osu_api_key

--- osu.py
+++ osu.py
@@ -3,10 +3,10 @@ import os
 
 # from api_key import api_key // No longer needed
 
-api_key = os.environ.get("OSU_API_KEY")
+osu_api_key = os.environ.get("OSU_API_KEY")
 
 
 def get_user_info(user):
-    payload = {"k": api_key, "u": user}
+    payload = {"k": osu_api_key, "u": user}
     r = requests.get("https://osu.ppy.sh/api/get_user", params=payload)
     return r.json()[0]
