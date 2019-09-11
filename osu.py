5e807870b15086c495d333f133f1e256ef146c0d
Andrew Mariano <corim.corim@gmail.com>
2019-09-10 17:26:23 -0700

    Reformat with black

--- osu.py
+++ osu.py
@@ -1,11 +1,12 @@
 import requests
 import os
+
 # from api_key import api_key // No longer needed
 
-api_key = os.environ.get('OSU_API_KEY')
+api_key = os.environ.get("OSU_API_KEY")
 
 
 def get_user_info(user):
-    payload = {'k': api_key, 'u': user}
+    payload = {"k": api_key, "u": user}
     r = requests.get("https://osu.ppy.sh/api/get_user", params=payload)
     return r.json()[0]
